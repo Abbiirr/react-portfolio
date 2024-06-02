7ed0a34475cffbc894dc85c97c57c21d2e760450
Abir Hossain <abirhossain9@iut-dhaka.edu>
2024-06-02 20:45:38 +0600

    FEAT: Updates Home

--- react-portfolio/src/components/Home/Home2.jsx
+++ react-portfolio/src/components/Home/Home2.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../assets/avatar.svg";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import myImg from "../../assets/profile.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -8,82 +8,82 @@ import {
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import '../../css/Home2.css';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+
 
 function Home2() {
+  const navigate = useNavigate()
+  const handleSubmit = () => {
+    //your code
+    
+    navigate("/projects")
+  }
+
   return (
     <Container fluid className="home-about-section" id="about">
       <Container>
-        <Row>
-          <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+        <Row className="align-items-center">
+          <Col md={6} className="home-about-description">
+            <h1 className="home-about-title">
+              Hi, I'm <span className="highlight">Abir Hossain</span>
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
+              I'm a passionate developer with experience in building web applications using
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="highlight"> C++, Javascript, and Java.</b>
               </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
+              <br /><br />
+              I specialize in creating modern web technologies and products, with a keen interest in
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to <b className="purple">Blockchain.</b>
+                <b className="highlight"> Game Development, AI and Distributed Systems.</b>
               </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              <br /><br />
+              I love working with <b className="highlight">Java Spring, Node.js</b> and modern JavaScript frameworks like
               <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
+                <b className="highlight"> React.js</b>
               </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              <br /><br />
+              Feel free to explore my work and connect with me on social media.
             </p>
+            
+          <Button className="explore-button" onClick={handleSubmit}>
+            Explore My Work
+          </Button>
           </Col>
-          <Col md={4} className="myAvtar">
+          <Col md={6} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={myImg} className="img-fluid avatar-img" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
-        <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
+        <Row className="home-about-social">
+          <Col className="text-center">
+            <h2 className="social-title">Connect with me</h2>
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
                   href="https://github.com/Abbiirr"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <AiFillGithub />
                 </a>
               </li>
-
               <li className="social-icons">
                 <a
                   href="https://www.linkedin.com/in/abir-hossain-b6717b214/"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <FaLinkedinIn />
                 </a>
               </li>
-
+              {/* Add more social links here if needed */}
             </ul>
           </Col>
         </Row>
@@ -91,4 +91,5 @@ function Home2() {
     </Container>
   );
 }
+
 export default Home2;
--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
@@ -13,95 +13,70 @@ import {
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
 } from "react-icons/ai";
-
 import { CgFileDocument } from "react-icons/cg";
+import '../css/NavBar.css';
 
 function NavBar() {
-  const [expand, updateExpanded] = useState(false);
-  const [navColour, updateNavbar] = useState(false);
+  const [expand, setExpand] = useState(false);
+  const [navColour, setNavColour] = useState(false);
 
-  function scrollHandler() {
-    if (window.scrollY >= 20) {
-      updateNavbar(true);
-    } else {
-      updateNavbar(false);
+  useEffect(() => {
+    function scrollHandler() {
+      setNavColour(window.scrollY >= 20);
     }
-  }
-
-  window.addEventListener("scroll", scrollHandler);
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, []);
 
   return (
     <Navbar
       expanded={expand}
       fixed="top"
       expand="md"
-      className={navColour ? "sticky" : "navbar"}
+      className={navColour ? "navbar-sticky" : "navbar"}
     >
       <Container>
-      <Navbar.Brand href="/" className="d-flex">
-        <img src={logo} className="img-fluid logo" alt="brand" style={{ width: '100px', height: '100px' }} />
-      </Navbar.Brand>
-
+        <Navbar.Brand href="/" className="d-flex align-items-center">
+          <img src={logo} className="img-fluid logo" alt="brand" />
+        </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
-          onClick={() => {
-            updateExpanded(expand ? false : "expanded");
-          }}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </Navbar.Toggle>
+          onClick={() => setExpand(expand ? false : "expanded")}
+        />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+              <Nav.Link as={Link} to="/" onClick={() => setExpand(false)}>
+                <AiOutlineHome className="nav-icon" /> Home
               </Nav.Link>
             </Nav.Item>
-
             <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/about"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+              <Nav.Link as={Link} to="/about" onClick={() => setExpand(false)}>
+                <AiOutlineUser className="nav-icon" /> About
               </Nav.Link>
             </Nav.Item>
-
             <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/project"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />{" "}
-                Projects
+              <Nav.Link as={Link} to="/project" onClick={() => setExpand(false)}>
+                <AiOutlineFundProjectionScreen className="nav-icon" /> Projects
               </Nav.Link>
             </Nav.Item>
-
             <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+              <Nav.Link as={Link} to="/resume" onClick={() => setExpand(false)}>
+                <CgFileDocument className="nav-icon" /> Resume
               </Nav.Link>
             </Nav.Item>
-
-
+            <Nav.Item>
+              <Nav.Link href="https://yourbloglink.com" target="_blank">
+                <ImBlog className="nav-icon" /> Blog
+              </Nav.Link>
+            </Nav.Item>
             <Nav.Item className="fork-btn">
               <Button
                 href="https://github.com/Abbiirr/react-portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
+                <CgGitFork className="fork-icon" /> <AiFillStar className="star-icon" />
               </Button>
             </Nav.Item>
           </Nav>
